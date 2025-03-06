952bffd26691d239ddf908d225365dab9cf95dca
Tobi3010 <tobiasandersen62@gmail.com>
2025-03-06 09:46:09 +0100

    preprocessing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
 import csv
+import nltk
+import string
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
 
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+
+# Functions for data loading ----------------------------------------------------------------------------------
 def load_data(filename):
     with open(filename, newline='', encoding='utf-8') as file:
         data = csv.DictReader(file)  # Automatically uses first row as headers
@@ -9,16 +21,57 @@ def load_data(filename):
 
 
 
+# Functions for data preprocessing ----------------------------------------------------------------------------
+def remove_uppercase(text):
+    return text.lower()
+
+def remove_numbers(text):
+    text = re.sub(r'\d+', '', text)
+    return text
+
+def remove_punctuation(text):
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
+def remove_punctuation_numbers(text):
+    return re.sub(r'[^\w\s]', '', text)
+
+def remove_whitespace(text):
+    return  " ".join(text.split())
+
+def remove_stopwords(text):
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(text)
+    filtered_text = [word for word in word_tokens if word not in stop_words]
+    return filtered_text
+
+def stem_words(text):
+    stems = [stemmer.stem(word) for word in text]
+    return stems
+
+def lemma_words(text):
+    lemmas = [lemmatizer.lemmatize(word) for word in text]
+    return lemmas
+
+
+
+# ------------------------------------------------------------------------------------------------------------
 
 filename1 = "data/lyrics/tcc_ceds_music.csv"
 filename2 = "data/lyrics/lyrics.csv"
 filename3 = "data/lyrics/tcc_ceds_music.csv"
-lyrics_list = load_data(filename2)
+textdata = load_data(filename2)
 
 # Example: Print the first lyric dictionary
-if lyrics_list:
-    print(len(lyrics_list))
-    print(lyrics_list[362235]["lyrics"])  # Prints the first song's data as a dictionary
+if textdata:
+    text = textdata[0]["lyrics"]
+    text = remove_punctuation_numbers(text)
+    text = remove_uppercase(text)
+    text = remove_stopwords(text)
+    text = lemma_words(text)
+    
+    print("\n Lyrics After Preprocessing ---------------------------------- \n")
+    print(text) 
 else:
     print("No data found.")
 
